620032813422cc29b8caf8f9e22cfca3ecc9d20e
Eduardo Henrique <edu.ed322@gmail.com>
2022-05-27 12:47:02 -0300

    update

--- ts/gastos.ts
+++ ts/gastos.ts
@@ -2,13 +2,15 @@ function reduce(a: number, b: number) {
   return a + b;
 }
 
-const nubank = [370.85, 35815.35].reduce(reduce);
-const santander = [21.5].reduce(reduce);
-const bb = [72.06].reduce(reduce);
-const itau = [12.6].reduce(reduce);
-const easy = [7621.63].reduce(reduce);
-const negative = [927.62,227.38,139.59, 1214.14,341.74*2].map((i) => -i).reduce(reduce);
-const waiting = [5000, 5000, 1300,2000].reduce(reduce); // carro, celio, licenciamento
+const nubank = [5.62, 24481.95].reduce(reduce);
+const santander = [676.98].reduce(reduce);
+const bb = [72.47].reduce(reduce);
+const itau = [87.55].reduce(reduce);
+const easy = [10029.98].reduce(reduce);
+const negative = [4000 - 1326.94, 2730 - 1470.77, 11000 - 8859.13]
+  .map((i) => -i)
+  .reduce(reduce);
+const waiting = [5000, 3000, 1300, 2000].reduce(reduce); // carro, celio, licenciamento
 
 const sum = Number((nubank + santander + bb + itau + easy).toFixed(2));
 
